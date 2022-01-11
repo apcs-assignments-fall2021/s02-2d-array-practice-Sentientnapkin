e81ad2cc6f7014700dbf562eb16649b1f39b99f3
Sentientnapkin <24svargas@athenian.org>
2022-01-11 10:25:57 -0800

    done

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,15 +1,28 @@
+import java.util.*;
 public class MyMain {
 
     // Returns the number of odd numbers in mat
     public static int countOdds(int[][] mat) {
-        // YOUR CODE HERE
-        return -1;
+        int count = 0;
+        for(int i = 0;i<mat.length;i++){
+            for(int j = 0;j<mat[i].length;j++){
+                if(mat[i][j]%2!=0)
+                    count++;
+            }
+        }
+        return count;
     }
 
     // Returns the largest value in mat
     public static int findLargest(int[][] mat) {
-        // YOUR CODE HERE
-        return -1;
+        int largest = Integer.MIN_VALUE;
+        for(int i = 0;i<mat.length;i++){
+            for(int j = 0;j<mat[i].length;j++){
+                if(mat[i][j]>largest)
+                    largest = mat[i][j];
+            }
+        }
+        return largest;
     }
 
     // Write a method that "left-shifts" each row in a 2D array.
@@ -17,15 +30,26 @@ public class MyMain {
     // shifted to the left by 1, with wraparound (the first value
     // should wrap around to the last entry in the row)
     public static int[][] leftShift(int[][] mat) {
-        // YOUR CODE HERE
-        return null;
+        int placeholder;
+        for(int i = 0;i<mat.length;i++){
+            placeholder = mat[i][0];
+            if (mat[i].length - 1 >= 0)
+                System.arraycopy(mat[i], 1, mat[i], 0, mat[i].length - 1);
+            mat[i][mat[i].length-1]=placeholder;
+        }
+        return mat;
     }
 
     // Creates a 2D array of size arraySize x arraySize
     // where each value is each to the sum of its row and col indexes
     public static int[][] arrayBuilder(int arraySize) {
-        // YOUR CODE HERE
-        return null;
+        int[][] mat = new int[arraySize][arraySize];
+        for(int i = 0;i<arraySize;i++){
+            for(int j = 0;j<arraySize;j++){
+                mat[i][j]=i+j;
+            }
+        }
+        return mat;
     }
 
 
@@ -41,23 +65,58 @@ public class MyMain {
 
     // Returns the mean of the 2D array mat
     public static double mean(double[][] mat) {
-        // YOUR CODE HERE
-        return -1.0;
+        int total  = 0;
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[i].length;j++){
+                total+=mat[i][j];
+            }
+        }
+        return (double)total/(mat.length*mat[0].length);
     }
 
     // Returns the median of the 2D array mat
     // Remember that the array is sorted!
     public static double median(double[][] mat) {
-        // YOUR CODE HERE
-        return -1.0;
+        ArrayList<Double> flat = new ArrayList<>();
+        for(int i = 0;i<mat.length;i++){
+            for(int j = 0;j<mat[i].length;j++){
+                flat.add(mat[i][j]);
+            }
+        }
+        if((flat.size()%2)!=0){
+            return flat.get(flat.size()/2);
+        }
+        else return (flat.get(flat.size()/2-1)+flat.get(flat.size()/2))/2;
     }
 
 
     // Returns the mode of the 2D array mat
     // Remember that the array is sorted!
     public static double mode(double[][] mat) {
-        // YOUR CODE HERE
-        return -1.0;
+        int frequency = 1;
+        int maxFrequency = 0;
+        double modeNum = 0;
+        for(int i = 0; i < mat.length; i++) {
+            for(int j = 0; j < mat[i].length-1; j++) {
+                if(mat[i][j]==mat[i][j+1]){
+                    frequency++;
+                    if(frequency>maxFrequency){
+                        modeNum=mat[i][j];
+                        maxFrequency=frequency;
+                    }
+                }
+                else frequency=1;
+            }
+            if (i<mat.length-1&&mat[i][mat[0].length-1]==mat[i+1][0]){
+                frequency++;
+                if(frequency>maxFrequency){
+                    modeNum=mat[i][mat[0].length-1];
+                    maxFrequency=frequency;
+                }
+            }
+            else frequency=1;
+        }
+        return modeNum;
     }
 
 
